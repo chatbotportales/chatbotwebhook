9a1aba6ae76192e263dee33e1ad9d93986a96b82
danimora2027 <reparacionphonecel@gmail.com>
2024-08-09 20:51:06 -0500

    inicio webhook chatbot

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+app.get('/', function(req, res) {
+    res.send('Hello World')
+})
+
+app.post('/webhook', express.json(), function(req, res) {
+    const agent = new WebhookClient({ request: req, response: res });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
+
+    function welcome(agent) {
+        agent.add(`Welcome to my agent!`);
+    }
+
+    function fallback(agent) {
+        const messages = [
+            `Ups, no he entendido a que te refieres.`,
+            `¿Podrías repetirlo, por favor?`,
+            `¿Disculpa?`,
+            `¿Decías?`,
+            `¿Cómo?`,
+            `¿No entiendo? Escribe 'Hola' para interactuar`
+        ];
+        const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+        agent.add(randomMessage);
+    }
+
+    function ProbandoWebhook(agent) {
+        agent.add(`Estoy enviando esta respuesta desde el webhook`);
+    }
+
+    function PortalesInteractivos(agent) {
+        agent.add(`Estos son los portales interactivos de ciudad bolivar`);
+    }
+
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('ProbandoWebhook', ProbandoWebhook);
+    intentMap.set('PortalesInteractivos', PortalesInteractivos);
+    agent.handleRequest(intentMap);
+})
+app.listen(3000, () => {
+    console.log("Estamos probando el puerto " + 3000);
+})
