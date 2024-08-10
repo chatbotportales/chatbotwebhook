0d761d5ffcdb887141236b0fcb4ffaee3ff4809e
danimora2027 <reparacionphonecel@gmail.com>
2024-08-09 22:18:00 -0500

    ajustes en index

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebase-adminsdk.json'); // Ruta al archivo de configuración
 const app = express()
 const { WebhookClient } = require('dialogflow-fulfillment');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://chatbotportalesinteracti-52820-default-rtdb.firebaseio.com"
+});
+
+const db = admin.firestore();
 
 app.get('/', function(req, res) {
     res.send('Hello World')
@@ -28,9 +37,20 @@ app.post('/webhook', express.json(), function(req, res) {
         agent.add(randomMessage);
     }
 
-    function ProbandoWebhook(agent) {
-        const frase = agent.parameters.pregunta;
-        agent.add(`Estoy enviando esta respuesta desde el ProbandoWebhook ` + frase);
+    async function ProbandoWebhook(agent) {
+
+        try {
+            const snapshot = await db.collection('portales').get();
+            const docs = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            res.json(docs);
+
+            const frase = agent.parameters.pregunta;
+            agent.add(`Estoy enviando esta respuesta desde el ProbandoWebhook ` + frase);
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error al obtener datos de Firestore' });
+        }
     }
 
     function PortalesInteractivos(agent) {
@@ -45,6 +65,30 @@ app.post('/webhook', express.json(), function(req, res) {
     intentMap.set('PortalesInteractivos', PortalesInteractivos);
     agent.handleRequest(intentMap);
 })
-app.listen(3000, () => {
+
+async function getPortalesAsText(collectionName) {
+    try {
+        const snapshot = await db.collection(collectionName).get();
+        const docs = snapshot.docs.map(doc => {
+            const data = doc.data();
+            return {
+                id: doc.id,
+                name: data.name // Asegúrate de que el campo 'name' existe en tus documentos
+            };
+        });
+
+        // Convertir los datos a texto
+        const textOutput = JSON.stringify(docs, null, 2); // Formateo con sangrías
+
+        return textOutput;
+    } catch (error) {
+        console.error('Error al obtener datos de Firestore:', error);
+        throw new Error('Error al obtener datos de Firestore');  
+    }
+}
+
+app.listen(3000, async() => {
+    const text = await getPortalesAsText('portales');
+    console.log(text)
     console.log("Estamos probando el puerto " + 3000);
 })
